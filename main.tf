bcea01beeaede8b679e6c96754a8364db803bfe3
ashprag7374 <131626729+ashprag7374@users.noreply.github.com>
2023-05-14 16:17:49 -0700

    update cidr

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_security_group_rule"  "blog_http_in" {
   from_port = "80"
   to_port = "80"
   protocol = "tcp"
-  cidr_blocks = "0.0.0.0/0"
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -54,7 +54,7 @@ resource "aws_security_group_rule"  "blog_https_in" {
   from_port = "443"
   to_port = "443"
   protocol = "tcp"
-  cidr_blocks = "0.0.0.0/0"
+   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -65,7 +65,7 @@ resource "aws_security_group_rule"  "blog_everythng_out" {
   from_port = 0
   to_port = 0
   protocol = "-1"
-  cidr_blocks = "0.0.0.0/0"
+   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
