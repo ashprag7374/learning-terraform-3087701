69f5824dfe821789c715bf0e7710daa65dd86e9a
ashprag7374 <131626729+ashprag7374@users.noreply.github.com>
2023-05-14 18:03:53 -0700

    fixed again

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
 
-  ingress_rules = ["http-80-tcp", "http-443-tcp"]
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0" ]
  
   egress_rules = ["all-all"]
