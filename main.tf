1d927c59c104c598d4c082a95ca1bd8ac718ce73
ashprag7374 <131626729+ashprag7374@users.noreply.github.com>
2023-05-14 21:34:06 -0700

    load balancer

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  #enable_nat_gateway = true
+  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
@@ -43,6 +43,49 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -55,5 +98,5 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0" ]
   
   name = "blog_new"
-  vpc_id = module.vpc.public_subnets[2]
+  vpc_id = module.vpc.vpc_id
 }
